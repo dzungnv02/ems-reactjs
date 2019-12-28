151cb27d19e15931b8d2e40d1b143d5edd238d14
Nguyen Vu Dung <dungnv@nal.vn>
2019-12-29 00:50:11 +0700

    fix docker-compose

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +16,8 @@ use Illuminate\Http\Request;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('/', function () {
+    return response()->json(['code' => 1, 'message' => 'OK'], 200);
 });
